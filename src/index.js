493f522d4879315e020cbcb34c3294ca3da92f19
pillai <satish.pillai888@gmail.com>
2021-02-27 13:32:20 +0530

    patch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import MainTable from './component/table';
-import MainPagination from './component/pagination';
+import MainTable from './component/table/index';
+import MainPagination from './component/pagination/index';
 import PaginationValue from './utils/calculateSkip';
 
 export {MainTable,MainPagination,PaginationValue};
